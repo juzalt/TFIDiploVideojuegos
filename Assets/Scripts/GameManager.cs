1f6098b27d89da3e8949d0a20e2c922545143838
Julian Zaltron <zaltron.julian@gmail.com>
2023-10-31 21:33:13 -0300

    Se generan los nodos

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int _width = 4;
+    [SerializeField] private int _height = 4;
+    [SerializeField] private Node _nodePrefab;
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                var node = Instantiate(_nodePrefab, new Vector2(x, y), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
