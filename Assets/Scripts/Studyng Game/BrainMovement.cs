4306c4d994e7ab82b52ce4ed8c29ee84938b7976
Mateo Astelarra <mateo@astelarra.com>
2023-11-08 11:27:23 -0300

    Implemented Brain Movement

--- Assets/Scripts/Studyng Game/BrainMovement.cs
+++ Assets/Scripts/Studyng Game/BrainMovement.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BrainMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private float maxSpeed = 1;
+    private Rigidbody2D rb;
+    private PlayerInput playerInput;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    public void Move()
+    {
+        Vector2 input = playerInput.actions["Move"].ReadValue<Vector2>();
+        if (input != Vector2.zero)
+        {
+            rb.AddForce(input.normalized * speed);
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        }
     }
 }
