6fd2dfd55c10df96349ec5f9b3e90ed3a47efc7a
Mateo Astelarra <mateo@astelarra.com>
2023-11-10 12:16:42 -0300

    Added Object Pool

--- Assets/Scripts/Studyng Game/Distraction.cs
+++ Assets/Scripts/Studyng Game/Distraction.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Distraction : MonoBehaviour
 {
     [SerializeField] private float distractionMagnitude = 1f;
     public float DistractionMagnitude { get => distractionMagnitude; }
+    
 
     [SerializeField] private float movementSpeed = 2f;
+
+    private IObjectPool<Distraction> distractionPool;
+    public IObjectPool<Distraction> DistractionPool { get => distractionPool; set => distractionPool = value; }
 
     void Start()
     {
@@ -20,5 +25,16 @@ public class Distraction : MonoBehaviour
     void Move()
     {
         transform.position += movementSpeed * Time.deltaTime * Vector3.right;
+    }
+
+    private void OnBecameInvisible()
+    {
+        if (gameObject.activeSelf)
+            DisableDistraction();
+    }
+
+    public void DisableDistraction()
+    {
+        distractionPool.Release(this);
     }
 }
--- Assets/Scripts/Studyng Game/Spawner.cs
+++ Assets/Scripts/Studyng Game/Spawner.cs
@@ -1,16 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] GameObject spawnable;
+    [SerializeField] Distraction distractionPrefab;
+    private IObjectPool<Distraction> distractionPool;
+
+    private void Awake()
+    {
+        distractionPool = new ObjectPool<Distraction>(
+            CreateDistraction,
+            OnGet,
+            OnRelease);
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(spawnable, transform.position, Quaternion.identity);
+            distractionPool.Get();
         }
+    }
+
+    private Distraction CreateDistraction()
+    {
+        Distraction distraction = Instantiate(distractionPrefab, transform.position, Quaternion.identity);
+        distraction.DistractionPool = distractionPool;
+        return distraction;
+    }
+
+    private void OnGet(Distraction distraction)
+    {
+        distraction.gameObject.SetActive(true);
+        distraction.transform.position = transform.position;
+    }
+
+    private void OnRelease(Distraction distraction)
+    {
+        distraction.gameObject.SetActive(false);
     }
 }
