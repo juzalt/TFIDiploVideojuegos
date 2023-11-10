8d6507ad12e2ea542d5650b672b925d92249f745
Mateo Astelarra <mateo@astelarra.com>
2023-11-10 10:50:46 -0300

    Added Distraction "Damage"

---
+++ Assets/Scripts/Studyng Game/BrainConcentration.cs
@@ -0,0 +1,33 @@
+
+using UnityEngine;
+
+public class BrainConcentration : MonoBehaviour
+{
+    [SerializeField] private float initialConcentration = 10f;
+
+    private float concentration;
+
+    void Start()
+    {
+        concentration = initialConcentration;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Distraction")
+        {
+            Distraction distraction = collision.gameObject.GetComponent<Distraction>();
+            LoseConcentration(distraction.DistractionMagnitude);
+        }
+    }
+
+    void LoseConcentration(float distractionMagnitude)
+    {
+        concentration -= distractionMagnitude;
+        Debug.Log(concentration);
+        if (concentration <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Studyng Game/Distraction.cs
+++ Assets/Scripts/Studyng Game/Distraction.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Distraction : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float distractionMagnitude = 1f;
+
+    public float DistractionMagnitude { get => distractionMagnitude; }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
