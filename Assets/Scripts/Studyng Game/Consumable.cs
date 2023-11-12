0b5c4b0d92bd5ec78487b371b3d1d8bdf22a5e45
Mateo Astelarra <mateo@astelarra.com>
2023-11-11 21:46:27 -0300

    Changed Knowledge initial direction movement

--- Assets/Scripts/Studyng Game/Consumable.cs
+++ Assets/Scripts/Studyng Game/Consumable.cs
@@ -14,11 +14,63 @@ public class Consumable : MonoBehaviour
     protected virtual void OnEnable()
     {
         DecideMovementSpeed();
+        SetInitialPositionAndMovementDirection();
     }
 
     void DecideMovementSpeed()
     {
         movementSpeed = Random.Range(movementSpeedMinMax.x, movementSpeedMinMax.y);
+    }
+
+    public void SetInitialPositionAndMovementDirection()
+    {
+        int side = Random.Range(0, 4);
+
+        Vector2 spawnPoint = Vector2.zero;
+        float spawnPointLooseCoordinate = Random.value;
+        float movementLooseDirection;
+
+        // Improve movement direction so distraction dont go directly out of the screen
+        if (spawnPointLooseCoordinate < 0.25f)
+        {
+            movementLooseDirection = Random.Range(0, 1f);
+        }
+        else if (spawnPointLooseCoordinate < 0.75f)
+        {
+            movementLooseDirection = Random.Range(-1f, 1f);
+        }
+        else
+        {
+            movementLooseDirection = Random.Range(-1f, 0f);
+        }
+
+        if (Random.value < 0.3) { movementLooseDirection = 0; } // Put it randomly vertical or Horizontal direction
+
+        switch (side)
+        {
+            case 0:
+                spawnPoint = new Vector2(0, spawnPointLooseCoordinate);
+                movementDirection = new Vector3(1, movementLooseDirection, 0);
+                break;
+            case 1:
+                spawnPoint = new Vector2(1, spawnPointLooseCoordinate);
+                movementDirection = new Vector3(-1, movementLooseDirection, 0);
+                break;
+            case 2:
+                spawnPoint = new Vector2(spawnPointLooseCoordinate, 0);
+                movementDirection = new Vector3(movementLooseDirection, 1, 0);
+                break;
+            case 3:
+                spawnPoint = new Vector2(spawnPointLooseCoordinate, 1);
+                movementDirection = new Vector3(movementLooseDirection, -1, 0);
+                break;
+        }
+
+        Vector3 worldSpawnPoint = Camera.main.ViewportToWorldPoint(spawnPoint);
+
+        worldSpawnPoint.z = 0;
+
+        gameObject.transform.position = worldSpawnPoint;
     }
 
     private void OnBecameInvisible()
--- Assets/Scripts/Studyng Game/Knowledge.cs
+++ Assets/Scripts/Studyng Game/Knowledge.cs
@@ -8,7 +8,6 @@ public class Knowledge : Consumable
     protected override void OnEnable()
     {
         base.OnEnable();
-        SetInitialPositionAndMovementDirection();
     }
 
     void Update()
@@ -21,36 +20,4 @@ public class Knowledge : Consumable
         transform.position += movementSpeed * Time.deltaTime * movementDirection;
     }
 
-    public void SetInitialPositionAndMovementDirection()
-    {
-        int side = Random.Range(0, 4);
-
-        Vector2 spawnPoint = Vector2.zero;
-
-        switch (side)
-        {
-            case 0:
-                spawnPoint = new Vector2(0, Random.value);
-                movementDirection = new Vector3(1, Random.Range(-1f, 1f), 0);
-                break;
-            case 1:
-                spawnPoint = new Vector2(1, Random.value);
-                movementDirection = new Vector3(-1, Random.Range(-1f, 1f), 0);
-                break;
-            case 2:
-                spawnPoint = new Vector2(Random.value, 0);
-                movementDirection = new Vector3(Random.Range(-1f, 1f), 1, 0);
-                break;
-            case 3:
-                spawnPoint = new Vector2(Random.value, 1);
-                movementDirection = new Vector3(Random.Range(-1f, 1f), -1, 0);
-                break;
-        }
-
-        Vector3 worldSpawnPoint = Camera.main.ViewportToWorldPoint(spawnPoint);
-
-        worldSpawnPoint.z = 0;
-
-        gameObject.transform.position = worldSpawnPoint;
-    }
 }
