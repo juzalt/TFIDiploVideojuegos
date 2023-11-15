796a1d08fa050d505f04568caf04a87879dbb74d
Mateo Astelarra <mateo@astelarra.com>
2023-11-15 12:57:33 -0300

    Added rotation animation (needs some adjustments)

--- Assets/Scripts/MainStage/PlayerInteractions.cs
+++ Assets/Scripts/MainStage/PlayerInteractions.cs
@@ -8,6 +8,8 @@ public class PlayerInteractions : MonoBehaviour
     NavMeshAgent rudy;
     private Animator animator;
     private Vector3 destination;
+    private Vector3 currentPosition;
+    
 
     void Start()
     {
@@ -31,16 +33,36 @@ public class PlayerInteractions : MonoBehaviour
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
             {
                 rudy.destination = hit.point;
+                currentPosition = transform.position;
+                rudy.updatePosition = false;
                 destination = hit.point;
             }
         }
+
+        var turnAngle = Vector3.Angle(transform.forward, destination - transform.position);
+        if (turnAngle >= 30)
+        {
+            var cross = Vector3.Cross(transform.forward, destination - transform.position);
+            animator.SetFloat("Turn", cross.normalized.y);
+        }
+        else
+        {
+            if (!rudy.updatePosition)
+            {
+                rudy.updatePosition = true;
+                rudy.nextPosition = currentPosition;
+                rudy.ResetPath();
+                rudy.destination = destination;    
+            }   
+        }
+
         animator.SetFloat("Velocity", Mathf.Clamp01(rudy.velocity.magnitude));
         //animator.SetBool("isWalking", rudy.velocity != Vector3.zero);
 
-        if (Vector3.Distance(transform.position, destination) < 0.1)
-        {
+        //if (Vector3.Distance(transform.position, destination) < 0.1)
+        //{
            // animator.SetBool("isWalking", false);
-        }
+        //}
     }
 
     private void InteractWithObjects()
