b0f4af470d75466ca9a539e391202a97c413a178
Julian Zaltron <zaltron.julian@gmail.com>
2023-11-30 19:19:57 -0300

    Popup inicial solo se ejecuta una vez. Cambios de texto guion.

--- Assets/Scripts/MainStage/ClosePopup.cs
+++ Assets/Scripts/MainStage/ClosePopup.cs
@@ -1,11 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ClosePopup : MonoBehaviour
 {
+    static bool openedBefore = false;
+
+    public void Awake()
+    {
+        Debug.Log("openedBefore::" + openedBefore);
+        if (openedBefore)
+        {
+            this.gameObject.transform.parent.gameObject.SetActive(false);
+        }
+    }
+
     public void Close()
     {
         this.gameObject.transform.parent.gameObject.SetActive(false);
+        openedBefore = true;
     }
 }
